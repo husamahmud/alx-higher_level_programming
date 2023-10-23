40f12d933cacb4dae66578acca42f2f792b99221
iamhusam <husamahmud@gmail.com>
2023-10-23 20:27:31 +0300

    Add 4->6

--- 0x05-python-exceptions/main.py
+++ 0x05-python-exceptions/main.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-safe_print_division = __import__('3-safe_print_division').safe_print_division
+raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
 
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
