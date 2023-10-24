403faed911a0527bdf686d8daf910505651103be
iamhusam <husamahmud@gmail.com>
2023-10-24 17:55:51 +0300

    Add 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square:
+    """Represents a squars."""
+
+    def __init__(self, size):
+        """Initializes a square with the given size."""
+        self.__size = size
