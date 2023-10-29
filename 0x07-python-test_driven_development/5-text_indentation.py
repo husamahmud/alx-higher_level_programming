d4a04893c4ea907a2436f754713f7e767e598b40
iamhusam <husamahmud@gmail.com>
2023-10-29 17:32:45 +0300

    Add 5

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: `.` `?` `:`.
+
+    Args:
+        text (str): The input text to be processed and printed. 
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: If the input `text` is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+
+    special_chars = ['.', '?', ':']
+    lines = []
+    curr_line = ''
+
+    for char in text:
+        curr_line += char
+        if char in special_chars:
+            lines.append(curr_line.strip())
+            lines.append('')
+            curr_line = ''
+
+    if curr_line:
+        lines.append(curr_line.strip())
+    for line in lines:
+        print(line)
