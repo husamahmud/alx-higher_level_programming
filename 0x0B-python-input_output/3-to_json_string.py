973a5ea1cceb5158e2c047c06075060aadae7cd6
iamhusam <husamahmud@gmail.com>
2023-11-08 00:52:47 +0300

    Add 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A script to return the JSON representation of an object"""
+import json
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object"""
+    json.dumps(my_obj)
