7eed91db03d492b9913dba1f59c06b2bb88e803c
iamhusam <husamahmud@gmail.com>
2023-11-07 18:52:11 +0300

    Add 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A script to writes a string to a text file."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file."""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        f.write(text)
+        return len(text)
