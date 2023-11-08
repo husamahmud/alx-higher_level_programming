cd4cff049450df794be852753eee966a7c1d4b68
iamhusam <husamahmud@gmail.com>
2023-11-08 03:34:01 +0300

    Add 1

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Student class."""
+
+
+class Student:
+    """A class that represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of the Student instance."""
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student."""
+        for key, val in json.items():
+            setattr(self, key, val)
