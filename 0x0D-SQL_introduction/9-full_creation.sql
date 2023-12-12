749bcdfa80686eff76ebafd0fd50e600d0a2809d
husamahmud <husamahmud@gmail.com>
2023-12-12 18:55:33 +0300

    Edit 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- creates a table second_table in the database and add multiples rows
 CREATE TABLE
-	IF NOT EXISTS second_table (
+	IF NOT EXISTS hbtn_0c_0.second_table (
 		id INT AUTO_INCREMENT,
 		name VARCHAR(256),
 		score INT,
@@ -8,7 +8,7 @@ CREATE TABLE
 	);
 
 INSERT INTO
-	second_table (name, score)
+	hbtn_0c_0.second_table (name, score)
 VALUES
 	('John', 10),
 	('Alex', 3),
