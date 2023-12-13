b12cccd1f2b32581ebf0594a928873d62c4780b4
husamahmud <husamahmud@gmail.com>
2023-12-13 19:40:02 +0300

    Edit 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -8,6 +8,6 @@ FROM
 WHERE
 	tv_shows.id = tv_show_genres.show_id
 	AND tv_show_genres.genre_id = tv_genres.id
-	AND tv_genres.name = "Comedy"
+	AND tv_genres.name = 'Comedy'
 ORDER BY
 	tv_shows.title ASC;
