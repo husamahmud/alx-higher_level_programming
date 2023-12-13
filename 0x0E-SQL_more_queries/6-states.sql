070dbe8236ca194ef9df7aef79b162e83633439e
husamahmud <husamahmud@gmail.com>
2023-12-13 17:09:05 +0300

    Edit 6
    Signed-off-by: husamahmud <husamahmud@gmail.com>

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE
-	IF NOT EXISTS states (
+	IF NOT EXISTS hbtn_0d_usa.states (
 		id INT AUTO_INCREMENT,
 		name VARCHAR(256) NOT NULL,
 		PRIMARY KEY (id)
