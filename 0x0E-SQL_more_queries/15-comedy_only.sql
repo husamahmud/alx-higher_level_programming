0605f828e691bd704118b67a976baf792ffb49be
husamahmud <husamahmud@gmail.com>
2023-12-13 19:38:55 +0300

    Edit 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -2,10 +2,12 @@
 SELECT
 	tv_shows.title
 FROM
-	tv_shows
-	INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-	INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+	tv_shows,
+	tv_show_genres,
+	tv_genres
 WHERE
-	tv_genres.name = 'Comedy'
+	tv_shows.id = tv_show_genres.show_id
+	AND tv_show_genres.genre_id = tv_genres.id
+	AND tv_genres.name = "Comedy"
 ORDER BY
-	tv_shows.title;
+	tv_shows.title ASC;
