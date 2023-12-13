8463de17fcabcc83b598ab1193c140aeb76758d2
husamahmud <husamahmud@gmail.com>
2023-12-13 16:39:26 +0300

    Edit 3->4

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -3,4 +3,4 @@ CREATE TABLE
 	IF NOT EXISTS force_name (
 		id INT,
 		name VARCHAR(256) NOT NULL,
-	)
+	);
--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -3,4 +3,4 @@ CREATE TABLE
 	IF NOT EXISTS id_not_null (
 		id INT NOT NULL DEFAULT 1,
 		name VARCHAR(256),
-	)
+	);
