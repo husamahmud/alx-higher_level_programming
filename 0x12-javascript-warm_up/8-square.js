12e3960e7d9b71b04ad067799c7a6ac958bba58d
husamahmud <husamahmud@gmail.com>
2024-01-08 19:01:47 +0300

    Add 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 const size = process.argv[2];
-if (isNaN(size === null)) {
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
-      console.log('X');
-    }
-    console.log();
+  for (let i = 0; i < parseInt(size); i++) {
+    console.log('X'.repeat(parseInt(size)));
   }
 }
