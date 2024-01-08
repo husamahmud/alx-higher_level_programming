ba4c5b7599564ef8a2a0a77ea11e67b1255840a5
husamahmud <husamahmud@gmail.com>
2024-01-08 18:29:14 +0300

    Add 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const num = process.argv[2];
-if (!num || !Number.isInteger(num)) {
+if (!num || !isNumber(num)) {
   console.log('Not a number');
 } else {
   console.log('My number:', num);
