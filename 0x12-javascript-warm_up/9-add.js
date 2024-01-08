c5157cfc2c304c441b9065371ccd78f22e161859
husamahmud <husamahmud@gmail.com>
2024-01-08 19:04:43 +0300

    Add 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 function add (a, b) {
-  return a + b;
+  return parseInt(a) + parseInt(b);
 }
 
-if (isNaN(process.argv[2]) || isNaN(process.argv[3])) {
-  console.log(NaN);
-} else {
-  console.log(add(process.argv[2], process.argv[3]));
-}
+console.log(add(process.argv[2], process.argv[3]));
