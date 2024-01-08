2df0625df075d36af14c45fa3fbb2d7a99216045
husamahmud <husamahmud@gmail.com>
2024-01-08 18:15:17 +0300

    Add 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('node:process');
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
