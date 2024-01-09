eca455b3dec199c117c5f4f3c4df9c444729f00d
husamahmud <husamahmud@gmail.com>
2024-01-09 15:49:20 +0300

    Add 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) return {};
+    if (w <= 0 || h <= 0) return new Rectangle();
 
     this.width = w;
     this.height = h;
