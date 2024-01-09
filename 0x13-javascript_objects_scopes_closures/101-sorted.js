88db377e63ab9073de3d76b8a796e8468d2f92c1
husamahmud <husamahmud@gmail.com>
2024-01-10 01:25:27 +0300

    Add 12

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,7 +3,9 @@ const { dict } = require('./101-data.js');
 
 const newDict = {};
 Object.entries(dict).forEach(([key, value]) => {
-  if (!newDict[value]) newDict[value] = [];
+  if (!newDict[value]) {
+    newDict[value] = [];
+  }
   newDict[value].push(key);
 });
 console.log(newDict);
