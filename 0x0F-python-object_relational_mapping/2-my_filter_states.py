3748892281d295ba36c9178575dc462e2e88536e
husamahmud <husamahmud@gmail.com>
2024-01-12 19:38:56 +0300

    Add 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     cursor = db.cursor()
     cursor.execute("""SELECT * FROM states
-                    WHERE name LIKE '{}'
+                    WHERE name LIKE BINARY '{}'
                     ORDER BY id""".format(argv[4]))
 
     rows = cursor.fetchall()
