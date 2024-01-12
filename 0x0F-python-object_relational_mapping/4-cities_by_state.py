eb6ca8fa9bed9c03382428ef0d4e504b242579f7
husamahmud <husamahmud@gmail.com>
2024-01-12 21:44:59 +0300

    Add 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument."""
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    connection = MySQLdb.connect(host="localhost",
+                                 port=3306,
+                                 user=argv[1],
+                                 passwd=argv[2],
+                                 db=argv[3])
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM cities ORDER BY id")
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    connection.close()
