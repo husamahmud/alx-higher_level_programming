82c52397403860d9c603327b87c1cca69ed8d397
husamahmud <husamahmud@gmail.com>
2024-01-12 21:42:20 +0300

    Add 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,20 +4,19 @@ hbtn_0e_0_usa where name matches the argument."""
 from sys import argv
 import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-
-    cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states ORDER BY id ASC""")
-
+if __name__ == '__main__':
+    connection = MySQLdb.connect(host="localhost",
+                                 port=3306,
+                                 user=argv[1],
+                                 passwd=argv[2],
+                                 db=argv[3])
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    
     rows = cursor.fetchall()
     for row in rows:
         if row[1] == argv[4]:
             print(row)
 
     cursor.close()
-    db.close()
+    connection.close()
