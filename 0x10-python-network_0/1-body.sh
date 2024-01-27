b5e603c80b0b1ec02111d767db9df25370b5705b
husamahmud <husamahmud@gmail.com>
2024-01-27 18:08:29 +0300

    Add 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-status_code=$(curl -s -o /dev/null -w "%{http_code}" "$1")
-
-if [ "$status_code" -eq 200 ]; then
-	body=$(curl -sL "$1")
-	echo "$body"
-fi
+curl -sL "$1"
