332115f522eed3f99b31d54a7584b32138472adb
husamahmud <husamahmud@gmail.com>
2024-01-28 19:14:11 +0300

    Add 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script that makes a request to 0.0.0.0:5000/catch_me that causes the server to
-# respond with a message containing You got me!, in the body of the response.
+# script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
 curl -sLX PUT -H origin:School 0.0.0.0:5000/catch_me_3
