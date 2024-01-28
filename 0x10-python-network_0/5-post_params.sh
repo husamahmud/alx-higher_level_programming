cbcb04c582040e191436e6b5424900c6c548538d
husamahmud <husamahmud@gmail.com>
2024-01-28 18:44:55 +0300

    Add 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-response=$(curl -X POST -sL \
-					-d "email=test@gmail.com&subject=I will always be here for PLD" \
-					--url "$1")
-
-echo "$response"
+curl -X POST -sd "email=test@gmail.com&subject=I will always be here for PLD" "$1"
