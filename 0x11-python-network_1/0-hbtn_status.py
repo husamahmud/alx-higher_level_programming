a9cfb272724a4c0f4d9e74c84ce971de6a1adb7d
husamahmud <husamahmud@gmail.com>
2024-01-30 19:07:08 +0300

    Add 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from urllib.request import urlopen
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    with urlopen(url) as res:
+        body = res.read()
+
+    print('Body response:')
+    print('- type:', type(body))
+    print('- content:', body)
+    print('- utf8 content:', body.decode('utf-8'))
+
+    res.close()
