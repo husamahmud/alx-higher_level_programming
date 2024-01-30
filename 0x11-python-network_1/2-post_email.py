ffd49a54a34553e9b04691a25168e66fa4a097bb
husamahmud <husamahmud@gmail.com>
2024-01-30 20:44:37 +0300

    Add 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response"""
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from sys import argv
+
+if __name__ == '__main__':
+    data = {'email': argv[2]}
+    req = Request(argv[1], urlencode(data).encode('utf-8'))
+
+    with urlopen(req) as res:
+        print('Your email is:', req)
