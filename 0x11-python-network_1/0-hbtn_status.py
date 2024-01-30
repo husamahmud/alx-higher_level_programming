d13787f14c1cd2f8c4be4c3c754571db0c797fa0
husamahmud <husamahmud@gmail.com>
2024-01-30 19:19:13 +0300

    Add 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,10 +7,9 @@ if __name__ == '__main__':
 
     with urlopen(url) as res:
         body = res.read()
-
-    print('Body response:')
-    print(f'\t- type: {type(body)}')
-    print(f'\t- content: {body}')
-    print(f'\t- utf8 content: {body.decode("utf-8")}')
+        print('Body response:')
+        print('\t- type:', type(body))
+        print('\t- content:', body)
+        print('\t- utf8 content:', body.decode("utf-8"))
 
     res.close()
