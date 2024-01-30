99da1f3e2aad410630e4ee56896094a2bf30e686
husamahmud <husamahmud@gmail.com>
2024-01-30 21:05:16 +0300

    Add 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,11 +3,15 @@
 URL with the email as a parameter, and displays the body of the response"""
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode
+
 from sys import argv
 
 if __name__ == '__main__':
-    data = {'email': argv[2]}
-    req = Request(argv[1], urlencode(data).encode('utf-8'))
+    url = argv[1]
+    email = argv[2]
+
+    data = urlencode({'email': email}).encode('utf-8')
+    req = Request(url, data=data, method='POST')
 
     with urlopen(req) as res:
         body = res.read().decode('utf-8')
