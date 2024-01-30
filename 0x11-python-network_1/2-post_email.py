3befdcd2ed981c7ef5eaef8351dbfef8c3d0a411
husamahmud <husamahmud@gmail.com>
2024-01-30 21:09:49 +0300

    Add 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,7 +3,6 @@
 URL with the email as a parameter, and displays the body of the response"""
 from urllib.request import urlopen, Request
 from urllib.parse import urlencode
-
 from sys import argv
 
 if __name__ == '__main__':
