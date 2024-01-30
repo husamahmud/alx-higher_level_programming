389e10b54447b9f63d657b3ae638dc68470d47fd
husamahmud <husamahmud@gmail.com>
2024-01-30 19:10:26 +0300

    Add 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
         body = res.read()
 
     print('Body response:')
-    print('    - type:', type(body))
-    print('    - content:', body)
-    print('    - utf8 content:', body.decode('utf-8'))
+    print('\t- type:', type(body))
+    print('\t- content:', body)
+    print('\t- utf8 content:', body.decode('utf-8'))
 
     res.close()
