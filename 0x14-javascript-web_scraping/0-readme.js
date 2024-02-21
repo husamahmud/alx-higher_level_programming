ef34121603817ddc27f30497c1c0d7f4a5a48d8d
Hüsam <husamahmud@gmail.com>
2024-02-21 04:44:12 +0300

    Add 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile('cisfun', 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) console.error(err);
   console.log(data);
 });
