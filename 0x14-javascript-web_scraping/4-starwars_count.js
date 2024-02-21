4962d6643742260b03e36ffb7aa193357f97b1d0
Hüsam <husamahmud@gmail.com>
2024-02-21 05:52:45 +0300

    Add 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,13 @@
 #!/usr/bin/node
 const request = require('request');
 
-const characterId = 18;
-
-request.get(process.argv[2], (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
+const wedgeAntilles = 'https://swapi-api.alx-tools.com/api/people/18/';
+let count = 0;
+request.get(process.argv[2], (err, res, body) => {
+  if (err) console.error(err);
+  const results = JSON.parse(body).results;
+  for (let i = 0; i < results.length; i++) {
+    if (results[i].characters.includes(wedgeAntilles)) count++;
   }
-
-  const films = JSON.parse(body).results;
-  let wedgeAppearances = 0;
-
-  films.forEach(film => {
-    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-      wedgeAppearances++;
-    }
-  });
-
-  console.log(wedgeAppearances);
+  console.log(count);
 });
