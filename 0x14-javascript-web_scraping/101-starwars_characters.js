082ba33c1c819e3d0f5a1ebae16ce90880d95f91
Hüsam <husamahmud@gmail.com>
2024-02-21 05:59:15 +0300

    Add 4->8

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://swapi-api.alx-tools.com/api/films/';
+
+function getCharacterNames(characters, index) {
+  if (index < characters.length) {
+    request.get(characters[index], (err, res, body) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const name = JSON.parse(body).name;
+        console.log(name);
+        getCharacterNames(characters, index + 1);
+      }
+    });
+  }
+}
+
+request.get(url + process.argv[2], (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const data = JSON.parse(body);
+    const characters = data.characters;
+    getCharacterNames(characters, 0);
+  }
+});
