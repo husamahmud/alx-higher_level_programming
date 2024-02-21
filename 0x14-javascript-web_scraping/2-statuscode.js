00546e28597a40d263d6a42468866e791ae69e5d
Hüsam <husamahmud@gmail.com>
2024-02-21 05:13:41 +0300

    Add 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,5 +3,5 @@ const request = require('request');
 
 request.get(process.argv[2], (err, res) => {
   if (err) console.error(err);
-  console.log(`code: ${res.statusCode}`);
+  else console.log(`code: ${res.statusCode}`);
 });
