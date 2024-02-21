92c5aa7295dbbf361a7c232506b8639508482c8f
Hüsam <husamahmud@gmail.com>
2024-02-21 04:34:17 +0300

    Add 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+const request = require('request');
+
 const file = process.argv[1];
-fetch(file)
-  .then(res => res.text())
-  .then(data => console.log(data))
-  .catch(err => console.error(err));
+request({ url: file, encoding: 'UTF-8' }, (err, res, body) => {
+  if (err) console.error(err);
+  else if (res.statusCode !== 200) console.error(`Request failed. ${res.statusCode}`);
+  else console.log(body);
+});
